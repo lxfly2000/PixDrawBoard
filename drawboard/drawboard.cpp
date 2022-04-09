3d450665fc4ae8181b409ec2507663eb631e56b0
Leaf <lxfly2000@outlook.com>
2022-04-09 21:30:14 +0800

    Add files via upload

---
+++ drawboard/drawboard.cpp
@@ -0,0 +1,327 @@
+﻿// drawboard.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "drawboard.h"
+#include "resource.h"
+#include <string>
+#include <queue>
+#include <CommCtrl.h>
+
+#define MAX_LOADSTRING 100
+#define WM_DRAWPIXEL (WM_USER+2000)
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+HWND hInputDialog = NULL;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DRAWBOARD, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DRAWBOARD));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DRAWBOARD));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DRAWBOARD);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   SendMessage(hWnd, WM_COMMAND, MAKEWPARAM(ID_MENU_OPEN_INPUT, 0), 0);
+   return TRUE;
+}
+
+std::queue<std::string>insq;
+
+void ExecuteInstructions(HWND hWnd,char* text)
+{
+	while (text&&*text)
+	{
+		if (*text >= '0'&&*text <= '9')
+		{
+			char*pEndlSpace = text;
+			for (; *pEndlSpace; pEndlSpace++)
+			{
+				if (*pEndlSpace == '\n' || *pEndlSpace == ' ')
+					break;
+			}
+			if (pEndlSpace == NULL)
+			{
+				insq.push(std::string(text));
+				text = pEndlSpace;
+			}
+			else
+			{
+				*pEndlSpace = NULL;
+				if (*(pEndlSpace - 1) == '\r')
+					*(pEndlSpace - 1) = NULL;
+				insq.push(std::string(text));
+				text = pEndlSpace + 1;
+			}
+		}
+		else
+		{
+			char *pn = strchr(text, '\n');
+			if (pn == NULL)
+			{
+				insq.push(std::string(text));
+				text = pn;
+			}
+			else
+			{
+				*pn = NULL;
+				if (*(pn - 1) == '\r')
+					*(pn - 1) = NULL;
+				insq.push(std::string(text));
+				text = pn + 1;
+			}
+		}
+	}
+	InvalidateRect(hWnd, NULL, TRUE);
+	UpdateWindow(hWnd);
+	SendMessage(hWnd, WM_DRAWPIXEL, 0, 0);
+}
+
+int scaleFactor = 1;
+
+INT_PTR CALLBACK InputDlgProcess(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		SendMessage(GetDlgItem(hWnd, IDC_SPIN1), UDM_SETRANGE32, 1, 32);
+		SetDlgItemInt(hWnd, IDC_EDIT_SCALE, 1, TRUE);
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			scaleFactor = GetDlgItemInt(hWnd, IDC_EDIT_SCALE, NULL, TRUE);
+			char *szBuf;
+			int n = GetWindowTextLength(GetDlgItem(hWnd, IDC_EDIT_INPUT)) + 1;
+			szBuf = (char*)malloc(sizeof(char)*n);
+			GetDlgItemTextA(hWnd, IDC_EDIT_INPUT, szBuf, n);
+			ExecuteInstructions(GetParent(hWnd), szBuf);
+			free(szBuf);
+		}
+			break;
+		case IDCANCEL:
+			EndDialog(hWnd, IDCANCEL);
+			break;
+		}
+		break;
+	}
+	return 0;
+}
+
+DWORD currentColor = 0xFFFFFFFF;
+
+COLORREF ColorToColorRef(DWORD c)
+{
+	return RGB((c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF);
+}
+
+void DrawPixel(HWND hWnd)
+{
+	std::string& strInst = insq.front();
+	if (strInst[0] >= '0'&&strInst[0] <= '9')//画图指令
+	{
+		char buf[16];
+		int row1, col1, row2, col2, colorIndex, type = 0;
+		for (char c : strInst)
+		{
+			if (c == '-')
+				type++;
+		}
+		if (type == 1)
+		{
+			sscanf_s(strInst.c_str(), "%d.%d-%d", &row1, &col1, &colorIndex);
+			row2 = row1;
+			col2 = col1;
+		}
+		else
+		{
+			sscanf_s(strInst.c_str(), "%d.%d-%d.%d-%d", &row1, &col1, &row2, &col2, &colorIndex);
+		}
+		HDC hdc = GetDC(hWnd);
+		COLORREF cref = ColorToColorRef(currentColor);
+		HBRUSH hbr = CreateSolidBrush(cref);
+		RECT rect{ col1*scaleFactor,row1*scaleFactor,(col2 + 1)*scaleFactor,(row2 + 1)*scaleFactor };
+		HGDIOBJ hOld=SelectObject(hdc, hbr);
+		FillRect(hdc, &rect, hbr);
+		SelectObject(hdc, hOld);
+		DeleteObject(hbr);
+		ReleaseDC(hWnd, hdc);
+	}
+	else//其他指令
+	{
+		if (strInst.substr(0, 4) == "颜色")
+			sscanf_s(strInst.c_str() + 5, "%08X", &currentColor);
+	}
+	insq.pop();
+	if (insq.size())
+		PostMessage(hWnd, WM_DRAWPIXEL, 0, 0);
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+			case ID_MENU_OPEN_INPUT:
+				hInputDialog = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG_INPUT), hWnd, InputDlgProcess);
+				ShowWindow(hInputDialog, SW_SHOW);
+				break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+	case WM_DRAWPIXEL:
+		DrawPixel(hWnd);
+		break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ pix2cmd/pix2cmd.cpp
@@ -0,0 +1,152 @@
+﻿//指令格式
+//尺寸 56
+//颜色 RRGGBB
+//单点上色：Y.X-0
+//区域上色：Y1.X1-Y2.X2-0
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <Windows.h>
+#include <gdiplus.h>
+#pragma comment(lib,"gdiplus.lib")
+
+struct Stroke
+{
+	int x1, y1, x2, y2;
+	Gdiplus::ARGB c;
+	std::string ToString()
+	{
+		std::ostringstream oss;
+		if (x1 == x2 && y1 == y2)
+			oss << y1 << "." << x1 << "-0";
+		else
+			oss << y1 << "." << x1 << "-" << y2 << "." << x2 << "-0";
+		return oss.str();
+	}
+	bool MergeHorizontal(const Stroke&other)
+	{
+		if (other.y1 != y1 || other.y2 != y2)
+			return false;
+		if (other.c != c)
+			return false;
+		if (min(x2, other.x2) - max(x1, other.x1) < -1)
+			return false;
+		x1 = min(x1, other.x1);
+		x2 = max(x2, other.x2);
+		return true;
+	}
+};
+std::map<Gdiplus::ARGB, std::vector<Stroke>>colorPalette;
+Gdiplus::ARGB bgColor = 0xFFFFFFFF;
+int instructionsPerLine = 1;
+
+std::string ColorToHTMLColor(Gdiplus::Color c)
+{
+	char fmt[7];
+	sprintf_s(fmt, "%02X%02X%02X", c.GetR(), c.GetG(), c.GetB());
+	return std::string(fmt);
+}
+
+std::vector<std::string> GenerateCommands(Gdiplus::Bitmap&bmp)
+{
+	using namespace Gdiplus;
+	UINT w = bmp.GetWidth(), h = bmp.GetHeight();
+	for (UINT i = 0; i < h; i++)
+	{
+		for (UINT j = 0; j < w; j++)
+		{
+			Color c;
+			bmp.GetPixel(j, i, &c);
+			ARGB argb = c.GetValue();
+			Stroke st{ j,i,j,i,argb };
+			if (argb != bgColor)
+			{
+				if (colorPalette[argb].empty())
+					colorPalette[argb].push_back(st);
+				else if (!colorPalette[argb].back().MergeHorizontal(st))
+					colorPalette[argb].push_back(st);
+			}
+		}
+	}
+	std::vector<std::string>v;
+	std::ostringstream oss;
+	oss << "尺寸 " << bmp.GetWidth();
+	v.push_back(oss.str());
+	for (auto p : colorPalette)
+	{
+		oss.str("");
+		oss << "颜色 " << ColorToHTMLColor(p.first);
+		v.push_back(oss.str());
+		for (auto k : p.second)
+			v.push_back(k.ToString());
+	}
+	return v;
+}
+
+DWORD HTMLColorToColor(wchar_t str[])
+{
+	DWORD dw;
+	swscanf_s(str, L"%08X", &dw);
+	return dw;
+}
+
+int wmain(int argc, wchar_t*argv[])
+{
+	if (argc < 2)
+	{
+		puts("命令行：pix2cmd <图片文件> <背景颜色(RGBA)=FFFFFFFF> <每行指令数=1>");
+		return 1;
+	}
+	if (argc >= 3)
+		bgColor = HTMLColorToColor(argv[2]);
+	if (argc >= 4)
+		instructionsPerLine = _wtoi(argv[3]);
+	using namespace std;
+	using namespace Gdiplus;
+	ULONG_PTR token;
+	GdiplusStartupInput gdipInput;
+	if (GdiplusStartup(&token, &gdipInput, NULL) != Ok)
+	{
+		puts("Gdiplus 加载失败。");
+		return -2;
+	}
+	{
+		//加载图像
+		Bitmap bmp(argv[1], FALSE);//这玩意不能跟GdiplusShutdown放一个括号里
+		if (bmp.GetLastStatus() != Ok)
+		{
+			puts("加载文件失败。");
+		}
+		else
+		{
+			cout << "宽：" << bmp.GetWidth() << " 高：" << bmp.GetHeight() << endl;
+			//生成指令
+			auto v = GenerateCommands(bmp);
+			cout << "一共" << v.size() << "条指令。" << endl;
+			int endlCounter = 0;
+			for (int i = 0; i < v.size(); i++)
+			{
+				if (v[i][0] >= '0'&&v[i][0] <= '9')
+				{
+					if (endlCounter == instructionsPerLine)
+						endlCounter = 0;
+					if (endlCounter == 0)
+						cout << endl;
+					else
+						cout << " ";
+					endlCounter++;
+				}
+				else
+				{
+					cout << endl;
+					endlCounter = 0;
+				}
+				cout << v[i];
+			}
+		}
+	}
+	GdiplusShutdown(token);
+	return 0;
+}
