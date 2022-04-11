9222191d8035eaac8bc291b67035e87838262089
Leaf <lxfly2000@outlook.com>
2022-04-11 09:57:20 +0800

    Update diffcmd.bat

--- diffcmd.bat
+++ diffcmd.bat
@@ -1,5 +1,4 @@
 @echo off
 curl https://lightningmaker.live/api/getimage?url=lightningmaker.live:5001 -o getimage.png
-mspaint getimage.png
-Release\imgdiff.exe flonne-pix-draw.png getimage.png
+Release\imgdiff.exe flonne-pix-draw.png getimage.png diff.png FFFFFFFF 1
 Release\pix2cmd.exe diff.png > diff.txt
